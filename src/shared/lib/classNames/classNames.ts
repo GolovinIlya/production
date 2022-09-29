14a3597ae99724eb0bfa107192787994c8ea7d81
Golovin Ilya <ilya.golovin.2001@mail.ru>
2022-09-29 13:52:08 +0300

    add jest

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,11 +1,28 @@
+<<<<<<< HEAD
 type Mods = Record<string, boolean | string>
 
 export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
+=======
+type Mods = Record<string, boolean | string>;
+
+export function classNames(
+    cls: string,
+    mods: Mods = {},
+    additional: string[] = []
+): string {
+>>>>>>> bad9de3 (add jest)
     return [
         cls,
         ...additional.filter(Boolean),
         ...Object.entries(mods)
+<<<<<<< HEAD
             .filter(([className, value]) => Boolean(value))
             .map(([className]) => className)
     ].join(' ');
 }
+=======
+            .filter(([_, value]) => Boolean(value))
+            .map(([className]) => className),
+    ].join(" ");
+}
+>>>>>>> bad9de3 (add jest)
