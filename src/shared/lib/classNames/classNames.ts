c5f67d1d39a0699e29723b2bc357d61a2064427f
Golovin Ilya <ilya.golovin.2001@mail.ru>
2022-10-05 11:04:24 +0300

    add jest

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-type Mods = Record<string, boolean | string>
-
-export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
-=======
 type Mods = Record<string, boolean | string>;
 
 export function classNames(
@@ -10,19 +5,11 @@ export function classNames(
     mods: Mods = {},
     additional: string[] = []
 ): string {
->>>>>>> bad9de3 (add jest)
     return [
         cls,
         ...additional.filter(Boolean),
         ...Object.entries(mods)
-<<<<<<< HEAD
-            .filter(([className, value]) => Boolean(value))
-            .map(([className]) => className)
-    ].join(' ');
-}
-=======
             .filter(([_, value]) => Boolean(value))
             .map(([className]) => className),
     ].join(" ");
 }
->>>>>>> bad9de3 (add jest)
--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,31 +1,11 @@
 import { Theme, useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-<<<<<<< HEAD
 import cls from "./ThemeSwitcher.module.scss";
-=======
->>>>>>> bad9de3 (add jest)
 import LightIcon from "shared/assets/icons/theme-light.svg";
 import DarkIcon from "shared/assets/icons/theme-dark.svg";
 import { Button, ThemeButton } from "shared/ui/Button/Button";
 
 interface ThemeSwitcherProps {
-<<<<<<< HEAD
-  className?: string;
-}
-
-export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
-  const { theme, toggleTheme } = useTheme();
-
-  return (
-    <Button
-      theme={ThemeButton.CLEAR}
-      onClick={toggleTheme}
-      className={classNames(cls.ThemeSwitcher, {}, [className])}
-    >
-      {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
-    </Button>
-  );
-=======
     className?: string;
 }
 
@@ -36,10 +16,9 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
         <Button
             theme={ThemeButton.CLEAR}
             onClick={toggleTheme}
-            className={classNames("", {}, [className])}
+            className={classNames(cls.ThemeSwitcher, {}, [className])}
         >
             {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
         </Button>
     );
->>>>>>> bad9de3 (add jest)
 };
