403ca17fbe13a8eeb36cf36338801e775da8fc0d
Golovin Ilya <ilya.golovin.2001@mail.ru>
2022-10-12 20:54:59 +0300

    add ErrorBoundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,21 +4,22 @@ import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 const App = () => {
-  const { theme } = useTheme();
-  return (
-    <div className={classNames("app", {}, [theme])}>
-      <Suspense fallback="">
-        <Navbar />
-        <div className="content-page">
-          <Sidebar />
-          <AppRouter />
+    const { theme } = useTheme();
+
+    return (
+        <div className={classNames("app", {}, [theme])}>
+            <Suspense fallback=''>
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
-      </Suspense>
-    </div>
-  );
+    );
 };
 
 export default App;
