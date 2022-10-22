b4a518dd848168a627dbb7b03aa6d7a0ec0bf7b5
Golovin Ilya <ilya.golovin.2001@mail.ru>
2022-10-22 17:31:47 +0300

    add storybook

---
+++ config/jest/setupTests.ts
@@ -0,0 +1,3 @@
+// Такой файл могли наблюдать при create-react-app
+import "@testing-library/jest-dom";
+import "regenerator-runtime/runtime";
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -5,26 +5,29 @@ import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import cls from "./Sidebar.module.scss";
 
 interface SidebarProps {
-  className?: string;
+    className?: string;
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
-  const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, setCollapsed] = useState(false);
 
-  const onToggle = () => {
-    setCollapsed((prev) => !prev);
-  };
-  return (
-    <div
-      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
-        className,
-      ])}
-    >
-      <button onClick={onToggle}>toggle</button>
-      <div className={cls.switchers}>
-        <ThemeSwitcher />
-        <LangSwitcher className={cls.lang} />
-      </div>
-    </div>
-  );
+    const onToggle = () => {
+        setCollapsed((prev) => !prev);
+    };
+    return (
+        <div
+            data-testid='sidebar'
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+                className,
+            ])}
+        >
+            <button data-testid='sidebar-toggle' onClick={onToggle}>
+                toggle
+            </button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+                <LangSwitcher className={cls.lang} />
+            </div>
+        </div>
+    );
 };
