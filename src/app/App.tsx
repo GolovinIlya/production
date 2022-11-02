9775f3cabcb1f1bd8d4ec60e5cd67943c9eec32c
Golovin Ilya <ilya.golovin.2001@mail.ru>
2022-11-02 19:59:49 +0300

    add Modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
-import { Suspense, useEffect } from "react";
+import { Suspense, useState } from "react";
 
 const App = () => {
     const { theme } = useTheme();
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,8 @@
+import { useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { Button, ButtonTheme } from "shared/ui/Button/Button";
+import { Modal } from "shared/ui/Modal/Modal";
 import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
@@ -10,21 +11,27 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink
-                    theme={AppLinkTheme.SECONDARY}
-                    to={"/"}
-                    className={cls.mainLink}
-                >
-                    {t("Главная")}
-                </AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>
-                    {t("О сайте")}
-                </AppLink>
-            </div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t("Войти")}
+            </Button>
+
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat
+                praesentium vel ea facilis aspernatur in ipsa rem. Quo, ratione
+                facere!
+            </Modal>
         </div>
     );
 };
