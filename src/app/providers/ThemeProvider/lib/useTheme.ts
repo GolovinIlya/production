ec63f3d673e9d70c50ae4190ee84953a44e36184
Golovin Ilya <ilya.golovin.2001@mail.ru>
2022-11-09 15:32:51 +0300

    fix theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -6,14 +6,15 @@ interface UseThemeResult {
     theme: Theme;
 }
 
-export function useTheme (): UseThemeResult {
+export function useTheme(): UseThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
 
-  const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK
-    setTheme(newTheme);
-    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
-  };
+    const toggleTheme = () => {
+        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        setTheme(newTheme);
+        document.body.className = newTheme;
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+    };
 
-  return {theme, toggleTheme}
+    return { theme, toggleTheme };
 }
