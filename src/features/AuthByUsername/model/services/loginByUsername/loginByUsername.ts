fb75fc7a7229c0467234fccfda60a015007dd903
Golovin Ilya <ilya.golovin.2001@mail.ru>
2022-11-20 13:29:52 +0300

    fix tests

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { User, userActions } from "entities/User";
-import i18n from "shared/config/i18n/i18n";
 import { USER_LOCALSTORAGE_KEY } from "shared/const/localStorage";
 
 interface LoginByUsernameProps {
@@ -33,8 +32,6 @@ export const loginByUsername = createAsyncThunk<
         return response.data;
     } catch (e) {
         console.log(e);
-        return thunkAPI.rejectWithValue(
-            i18n.t("Вы ввели неверный логин или пароль")
-        );
+        return thunkAPI.rejectWithValue("Вы ввели неверный логин или пароль");
     }
 });
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -9,6 +9,7 @@ import { loginActions } from "../../model/slice/LoginSlice";
 import { getLoginState } from "../../model/selectors/getLoginState/getLoginState";
 import { loginByUsername } from "../../model/services/loginByUsername/loginByUsername";
 import { Text, TextTheme } from "shared/ui/Text/Text";
+import i18n from "shared/config/i18n/i18n";
 
 interface LoginFormProps {
     className?: string;
@@ -42,7 +43,12 @@ export const LoginForm = memo(({ className }: LoginFormProps) => {
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
             <Text title={t("Форма авторизации")} />
-            {error && <Text text={error} theme={TextTheme.ERROR} />}
+            {error && (
+                <Text
+                    text={i18n.t("Вы ввели неверный логин или пароль")}
+                    theme={TextTheme.ERROR}
+                />
+            )}
             <Input
                 autofocus
                 type='text'
