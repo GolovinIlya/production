09b5586182163634785488245da096b8abcf7ff2
Golovin Ilya <ilya.golovin.2001@mail.ru>
2022-11-20 13:38:06 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,13 +1,15 @@
 import { fireEvent, screen } from "@testing-library/react";
+import { Sidebar } from "widgets/Sidebar/ui/Sidebar/Sidebar";
+import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
 import { componentRender } from "shared/lib/tests/componentRender/componentRender";
-import { Sidebar } from "./Sidebar";
 
 describe("Sidebar", () => {
-    test("Test render", () => {
+    test("with only first param", () => {
         componentRender(<Sidebar />);
         expect(screen.getByTestId("sidebar")).toBeInTheDocument();
     });
-    test("Test toggle", () => {
+
+    test("test toggle", () => {
         componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId("sidebar-toggle");
         expect(screen.getByTestId("sidebar")).toBeInTheDocument();
