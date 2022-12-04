d7cf0a9b759c69d505cda4500f3b413d5313f2fb
Golovin Ilya <ilya.golovin.2001@mail.ru>
2022-12-04 16:03:07 +0300

    add lazy loading add reducer manager

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,12 +1,22 @@
-import { DeepPartial } from "@reduxjs/toolkit";
+import { DeepPartial, ReducersMapObject } from "@reduxjs/toolkit";
 import { Story } from "@storybook/react";
 import { StateSchema, StoreProvider } from "app/providers/StoreProvider";
+import { loginReducer } from "features/AuthByUsername/model/slice/LoginSlice";
+
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+    loginForm: loginReducer,
+};
 
 export const StoreDecorator =
-    (state: DeepPartial<StateSchema>) => (StoryComponent: Story) => {
-        return (
-            <StoreProvider initialState={state}>
-                <StoryComponent />
-            </StoreProvider>
-        );
+    (
+        state: DeepPartial<StateSchema>,
+        asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+    ) =>
+    (StoryComponent: Story) => {
+        <StoreProvider
+            initialState={state}
+            asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
+        >
+            <StoryComponent />
+        </StoreProvider>;
     };
