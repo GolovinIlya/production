ab08b433bf2a4667a395849dcbd9565747295460
Golovin Ilya <ilya.golovin.2001@mail.ru>
2022-12-07 13:15:50 +0300

    add api

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -1,18 +1,15 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Theme } from "app/providers/ThemeProvider";
-
-import "app/styles/index.scss";
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
-import ProfilePage from "./ProfilePage";
+import { Theme } from "app/providers/ThemeProvider";
+import ProfilePage from "pages/ProfilePage/ui/ProfilePage";
+import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 export default {
     title: "pages/ProfilePage",
     component: ProfilePage,
     argTypes: {
         backgroundColor: { control: "color" },
-    },
-    args: {
-        to: "/",
     },
 } as ComponentMeta<typeof ProfilePage>;
 
@@ -22,8 +19,8 @@ const Template: ComponentStory<typeof ProfilePage> = (args) => (
 
 export const Normal = Template.bind({});
 Normal.args = {};
+Normal.decorators = [StoreDecorator({})];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
