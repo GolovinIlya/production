cc856e6c6eba36c90bfc3cea38efb71dbd0bb1e9
Golovin Ilya <ilya.golovin.2001@mail.ru>
2023-01-18 17:01:08 +0300

    check fails

--- src/entities/Counter/model/selectors/getCounterValue/getCounterValue.test.ts
+++ src/entities/Counter/model/selectors/getCounterValue/getCounterValue.test.ts
@@ -1,6 +1,6 @@
 import { DeepPartial } from "@reduxjs/toolkit";
 import { StateSchema } from "app/providers/StoreProvider";
-import { getCounterValue } from "./getCounetrValue";
+import { getCounterValue } from "./getCounterValue";
 
 describe("getCounterValue.test", () => {
     test("", () => {
