c00cb01c3656abc6edb31e829fafd59ff25d61b5
Golovin Ilya <ilya.golovin.2001@mail.ru>
2023-02-19 14:14:19 +0400

    fit typings

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -14,11 +14,10 @@ export const loginByUsername = createAsyncThunk<
     LoginByUsernameProps,
     ThunkConfig<string>
 >("login/loginByUsername", async (authData, thunkAPI) => {
+    const { extra, dispatch, rejectWithValue } = thunkAPI;
+
     try {
-        const response = await axios.post<User>(
-            "http://localhost:8000/login",
-            authData
-        );
+        const response = await extra.api.post<User>("/login", authData);
 
         if (!response.data) {
             throw new Error();
@@ -28,11 +27,12 @@ export const loginByUsername = createAsyncThunk<
             USER_LOCALSTORAGE_KEY,
             JSON.stringify(response.data)
         );
-        thunkAPI.dispatch(userActions.setAuthData(response.data));
+
+        dispatch(userActions.setAuthData(response.data));
 
         return response.data;
     } catch (e) {
         console.log(e);
-        return thunkAPI.rejectWithValue("error");
+        return rejectWithValue("error");
     }
 });
