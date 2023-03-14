ee65468ecaa84c42ef82075109c9a5573e749421
GolovinIlya <ilya.golovin.2001@mail.ru>
2023-03-14 17:21:07 +0400

    fit tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -4,6 +4,7 @@ import "app/styles/index.scss";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "app/providers/ThemeProvider";
 import { Sidebar } from "./Sidebar";
+import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 export default {
     title: "widget/Sidebar",
@@ -19,8 +20,27 @@ const Template: ComponentStory<typeof Sidebar> = (args) => (
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [
+    ThemeDecorator(Theme.LIGHT),
+    StoreDecorator({
+        user: { authData: {} },
+    }),
+];
 
 export const Dark = Template.bind({});
 Dark.args = {};
+Dark.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        user: { authData: {} },
+    }),
+];
 
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+export const NoAuth = Template.bind({});
+NoAuth.args = {};
+NoAuth.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        user: {},
+    }),
+];
